8b20983b806bb4581d61addadd048b8b83786c3d
OmegaXelix <miloskecojevic907@gmail.com>
2021-03-16 22:31:19 +0100

    commit sa kompa

--- oledText.py
+++ oledText.py
@@ -11,15 +11,8 @@ import json
 def do_nothing(obj):
   pass
 
-# rev.1 users set port=0
-# substitute spi(device=0, port=0) below if using that interface
-# substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
 serial = i2c(port=1, address=0x3C)
-
-# substitute ssd1331(...) or sh1106(...) below if using that device
 device = sh1106(serial)
-
-# ne cisti posle zavrsetka skripte
 device.cleanup = do_nothing
 
 #font = ImageFont.truetype("courier-prime.regular.ttf", 11);
